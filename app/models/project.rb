5f8f03f6221593beab3b210d280a59bb13c02ab2
Philip Engles <englep10@gmail.com>
2015-05-28 01:54:40 -0400

    Added Reviews

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,9 @@ class Project < ActiveRecord::Base
 	has_many :subscriptions
 	has_many :users, through: :subscriptions
 
+
+   	has_many :reviews
+
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :content, presence: true, length: { maximum: 500 }
 	validates :price, presence: true, numericality: { only_integer: true }
