9e3391df8a9b6e4f24415099fa8fa3914cdb8679
Philip Engles <englep10@gmail.com>
2015-05-28 16:39:32 -0400

    Refactored Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,6 @@ class Task < ActiveRecord::Base
 
 	validates :title, presence: true, length: { maximum: 500}
 	validates :video, presence: true
-	validates :tag, presence: true
 	validates :project, presence: true
 
 
