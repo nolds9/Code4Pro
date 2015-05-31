4c45a5a23909437584f21429c96d2387817a8939
Philip Engles <englep10@gmail.com>
2015-05-31 00:17:15 -0400

    Added New Receipt Transactional Email

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -22,4 +22,26 @@ class MyMailer < ActionMailer::Base
 
 	end
 
+	def new_receipt(user, project)
+		template_name = "new-receipt"
+		template_content = []
+		message = {
+			to: [{email: "#{user.email}"}],
+			subject: "Code4Pro -- Your Receipt",
+			merg_vars: [
+				{
+					rcpt: user.email,
+					vars: [
+						{name: "STUDENT_NAME", content: user.name},
+						{name: "PROJECT_NAME", content: project.name},
+						{name: "PROJECT_PRICE", content: project.price}
+					]
+				}
+			]
+		}
+
+		mandrill_client.messages.send_template template_name, template_content, message
+
+	end
+
 end
